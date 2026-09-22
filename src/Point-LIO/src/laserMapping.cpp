ae65dfda2b72c61b252120f6edc9ea26e3e6a279
shifeng <shifengddd@gmail.com>
2026-09-22 20:48:27 +0800

    与下位机通信

--- src/Point-LIO/src/laserMapping.cpp
+++ src/Point-LIO/src/laserMapping.cpp
@@ -256,7 +256,7 @@ void publish_odometry(
   if (tf_send_en) {
     geometry_msgs::msg::TransformStamped transform;
     transform.header.frame_id = "camera_init";
-    transform.child_frame_id = "aft_mapped";
+    transform.child_frame_id = "body";
     transform.transform.translation.x = odomAftMapped.pose.pose.position.x;
     transform.transform.translation.y = odomAftMapped.pose.pose.position.y;
     transform.transform.translation.z = odomAftMapped.pose.pose.position.z;
